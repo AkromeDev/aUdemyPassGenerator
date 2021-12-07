f89ebd0e351554840755a2111ef62a9fdba858b3
JCBackendDev <jcbackenddev@gmail.com>
2021-12-07 14:35:28 +0100

    First commit: Password Generator
    html Template has been created with a simple structure.
    component.ts takes care of displaying the password dynamically.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aUdemyPassGenerator';
+password = '';
+
+  onButtonClick() {
+    this.password = 'some Password';
+  }
 }
